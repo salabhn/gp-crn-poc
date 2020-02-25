a24fbea24fd26e614ac55617f7b75588969c1c4a
Salabh A. N <salabhn@growthplug.com>
2020-02-25 14:15:36 +0530

    testing openshift client

--- cron_test/signals.py
+++ cron_test/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from kubernetes import client, config
+from openshift.dynamic import DynamicClient
 import logging
 
 from .models import CronJob
@@ -9,5 +11,12 @@ logger = logging.getLogger('django')
 
 
 @receiver(post_save, sender=CronJob)
-def update_chargebee_add_on(sender, instance, created, **kwargs):
+def update_cron_job(sender, instance, created, **kwargs):
     logger.info('LOG WORKING')
+    config.load_incluster_config()
+    logger.info(dir(client))
+    v1 = client.CoreV1Api()
+    dyn_client = DynamicClient(v1)
+    v1_services = dyn_client.resources.get(api_version='v1', kind='Pod')
+    logger.info('PODS::: %s' % v1_services)
+    logger.info('PODS::: %s' % dir(v1_services))
